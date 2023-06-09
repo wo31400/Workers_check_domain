804ddf1d7c7f7049160fea856ca493ec67fa5b1e
yuansir <wo31400@qq.com>
2023-06-10 02:30:38 +0800

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,13 @@
+export default {
+  async fetch(request) {
+    console.log(request)
+    const init = {
+      headers: {
+        "Referer": "https://guanjia.qq.com",
+      },
+    };
+    const requestURL = new URL(request.url);
+    const path = requestURL.pathname.slice(1);
+    return fetch(`https://cgi.urlsec.qq.com/index.php?m=check&a=check&url=${path}`,init);
+  },
+};
